38b68dc4924a3c94d42eb090242afd9d1811ee57
fysq <501258965@qq.com>
2017-12-22 14:55:18 +0800

    x

--- js/function.js
+++ js/function.js
@@ -6,3 +6,33 @@ function checkphone(str) {
       return true;  
   }  
 }  
+
+function myreload(){
+    window.location.href=updateUrl(window.location.href);
+}
+
+function updateUrl(url,k,v,rule){
+    var rule= (eval('/('+ k+'=)([^&]*)/gi') || eval('/(t=)([^&]*)/gi'));  //默认是数字
+    // var rule= (rule || '(?=\&)');  //默认是数字
+    var k= (k || 't') +'=';  //默认是"t"
+    var v= (v || +new Date());  //默认是 时间戳
+    var reg=new RegExp(rule);  
+    if(url.indexOf(k)>-1){ 
+        return url.replace(reg,k+v);
+    }else{ 
+        if(url.indexOf('\?')>-1){
+            var urlArr=url.split('\?');
+            if(urlArr[1]){
+                return urlArr[0]+'?'+urlArr[1]+'&'+k+v;
+            }else{
+                return urlArr[0]+'?'+k+v;
+            }
+        }else{
+            if(url.indexOf('#')>-1){
+                return url.split('#')[0]+'?'+k+v;
+            }else{
+                return url+'?'+k+v;
+            }
+        }
+    }
+}
